5b1906d2fd9801c0f8806563e7a251f4148583ba
sfcoscia <sfcoscia2002@gmail.com>
2025-09-09 17:19:52 -0700

    plotting script

--- pyplot2.py
+++ pyplot2.py
@@ -1,16 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Read data from the text file
-# For formatted data, np.loadtxt is convenient
-data = np.loadtxt('./200/fort.19')
+# Load the Fortran data:
+data = np.loadtxt('./fort.7')
 
 # Separate x and y values
 x = data[:, 0]
-y = data[:, 2]
-print(x, y)
+y1 = data[:, 1]
+y2 = data[:, 2]
+y3 = data[:, 3]
+y4 = data[:, 4]
+y5 = data[:, 5]
+#print(x, y)
 # Create the plot
-plt.plot(x, y, marker='o', linestyle='-')
+plt.plot(x, y1, marker='o', linestyle='-')
+plt.plot(x, y2, marker='o', linestyle='-')
+plt.plot(x, y3, marker='o', linestyle='-')
+plt.plot(x, y4, marker='o', linestyle='-')
+plt.plot(x, y5, marker='o', linestyle='-')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Fortran Data Plot')
